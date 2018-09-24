8cdf7452ec6498c74fd918e218192439bb2e2518
jars0829 <jandresrsalas@gmail.com>
2018-09-24 00:50:13 -0300

    adding extracting functions

--- load_data.py
+++ load_data.py
@@ -17,4 +17,25 @@ def importing_data(filename, data):
         return key_data
 
 
-def extracting_data(eye_data, EEG_data, RST_data):
+def extracting_index(RST_data):
+    index_sent=[]
+    index_recall=[]
+    [index_sent.append(index) for index, value in enumerate(RST_data['time_series']) if value == [u'Start_sentence']]
+    [index_recall.append(index) for index, value in enumerate(RST_data['time_series']) if value == [u'Start_recall']]
+    stamps_sent = [RST_data['time_stamps'][index_sent],RST_data['time_stamps'][np.array(index_sent)+1]]
+    stamps_recall = [RST_data['time_stamps'][index_recall],RST_data['time_stamps'][np.array(index_recall)+1]]
+
+    return stamps_recall, stamps_sent
+
+def extracting_EEG(EEG_data, stamps):
+    size=np.shape(stamps)[1]
+
+def extracting_eye(eye_data, stamps):
+    size = np.shape(stamps)[1]
+    data={}
+    data_sentences={}
+    for i in range(size):
+        data['raw'] = []
+        while (True):
+            data['raw'].append(np.transpose())
+
