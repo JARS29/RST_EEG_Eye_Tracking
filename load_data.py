f38b07cac54d37efc109945e115899bc601b9698
JARS29 <jandresrsalas@gmail.com>
2018-09-24 14:39:06 -0300

    Adding extracting data function (EEG and Eye-data) and improving extracting index

--- load_data.py
+++ load_data.py
@@ -17,25 +17,40 @@ def importing_data(filename, data):
         return key_data
 
 
-def extracting_index(RST_data):
+def extracting_index(RST_data, data, type ):   # RST data for the ts of the sentences+recall, data for the indexes (either EEG or Eye-tracking), type= 'sentences' or 'recall'
     index_sent=[]
     index_recall=[]
     [index_sent.append(index) for index, value in enumerate(RST_data['time_series']) if value == [u'Start_sentence']]
     [index_recall.append(index) for index, value in enumerate(RST_data['time_series']) if value == [u'Start_recall']]
-    stamps_sent = [RST_data['time_stamps'][index_sent],RST_data['time_stamps'][np.array(index_sent)+1]]
-    stamps_recall = [RST_data['time_stamps'][index_recall],RST_data['time_stamps'][np.array(index_recall)+1]]
+    stamps_sent = [RST_data['time_stamps'][index_sent],RST_data['time_stamps'][np.array(index_sent)+1]]         # Start sentence, Final Sentence (indexes)
+    stamps_recall = [RST_data['time_stamps'][index_recall],RST_data['time_stamps'][np.array(index_recall)+1]]   # Start recall, Final recall (indexes)
+    index_beg = []
+    index_end = []
+    if type == 'sentences':
+        for i in range(np.shape(stamps_sent)[1]):
+            temp_1 = np.where(np.ceil(data['time_stamps']) == np.ceil(stamps_sent[0][i]))
+            temp_2 = np.where(np.ceil(data['time_stamps']) == np.ceil(stamps_sent[1][i]))
+            index_beg.append(temp_1[0][1])  #First item for beginning
+            index_end.append(temp_2[0][-1]) #Last item for ending
+        return index_beg, index_end
 
-    return stamps_recall, stamps_sent
+    if type == 'recall':
+        for i in range(np.shape(stamps_recall)[1]):
+            temp_1 = np.where(np.ceil(data['time_stamps']) == np.ceil(stamps_recall[0][i]))
+            temp_2 = np.where(np.ceil(data['time_stamps']) == np.ceil(stamps_recall[1][i]))
+            index_beg.append(temp_1[0][1])  #First item for beginning
+            index_end.append(temp_2[0][-1]) #Last item for ending
+        return index_beg, index_end
 
-def extracting_EEG(EEG_data, stamps):
-    size=np.shape(stamps)[1]
 
-def extracting_eye(eye_data, stamps):
-    size = np.shape(stamps)[1]
-    data={}
-    data_sentences={}
-    for i in range(size):
-        data['raw'] = []
-        while (True):
-            data['raw'].append(np.transpose())
+
+def extracting_data(data,beg,end):  # data (either EEG or Eye_data;
+    raw_data = {}
+    temp = {}
+    for i in range(len(end)):
+        temp['raw'] = data['time_series'][beg[i]:end[i]]
+        temp['ts'] = data['time_stamps'][beg[i]:end[i]]
+        raw_data[i + 1] = temp.copy()
+    return raw_data
+
 
